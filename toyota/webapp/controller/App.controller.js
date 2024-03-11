6b74836c9fa5079dbc1dab6b0c3cbfe798f8c802
Meda Kishan <meda.kishan@incture.com>
2024-03-11 17:51:34 +0000

    initial commit

---
+++ toyota/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("application.toyota.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ toyota/webapp/controller/button.controller.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function(
+	Controller,MessageToast
+) {
+	"use strict";
+
+	return Controller.extend("application.toyota.controller.button", {
+	
+    
+    call_frag()
+    {
+        this.dialog??=this.loadFragment(
+            {
+                name:"application.toyota.view.fragments.form"
+            }
+        );
+
+        this.dialog.then((odialog) => odialog.open())
+    },
+
+    closeDialog()
+    {
+        this.byId("contactusDialog").close();
+    },
+
+    onsubmit()
+    {
+        this.byId("contactusDialog").close();
+        MessageToast.show("Form submitted Successfully");
+
+    },
+
+    onimg_press()
+    {
+        window.open("https://www.google.com/maps/place/Incture+Technologies/@12.8434406,77.6626076,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae6c8634d83b39:0xc56f2a851f7b3ba4!8m2!3d12.8434406!4d77.6651825!16s%2Fg%2F1hm62knf2?entry=ttu")
+    }
+
+    });
+});
